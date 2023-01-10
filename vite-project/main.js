23b1e8365a713349ea2e4ece742d880943f9cf4b
michaelv24 <mvsch20@gmail.com>
2023-01-10 10:15:47 -0500

    work on the website

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,8 @@
 import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+};
+console.log(DOMSelectors.button);
