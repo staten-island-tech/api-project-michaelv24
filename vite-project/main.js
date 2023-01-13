f251dbe2a0da392c6d126ed918fda63fa2e82ecf
michaelv24 <mvsch20@gmail.com>
2023-01-13 10:14:46 -0500

    API

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,8 +1,20 @@
 import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
+const URL = "https://botw-compendium.herokuapp.com/api/v2${endpoint}?foo=foo";
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json(); //makes the object into JSON object we can use
+    document.getElementById("api-response").textContent = data.textContent;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
 const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
+  input: document.getElementById("input"),
 };
 console.log(DOMSelectors.button);
