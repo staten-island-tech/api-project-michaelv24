039b37ec577ea046f6a87c734ff52b860e019215
michaelv24 <mvsch20@gmail.com>
2023-02-01 00:30:18 -0500

    card

---
+++ vite-project/js/DOMSelectors.js
@@ -0,0 +1,7 @@
+export const DOMSelectors = {
+  button: document.querySelector(".btn"),
+  page: document.querySelector(".main"),
+  title: document.querySelector(".name"),
+  info: document.querySelector(".info"),
+  display: document.getElementById("app"),
+};
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,38 +1,77 @@
-<<<<<<< Updated upstream
-import "../styles``./style.css";
-=======
 import "../styles/style.css";
->>>>>>> Stashed changes
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "../counter.js";
-const URL = "https://botw-compendium.herokuapp.com/api/v2";
+import { DOMSelectors } from "../js/DOMSelectors.js";
+
+const apiURL = "https://botw-compendium.herokuapp.com/api/v2/entry/";
+
 async function Data(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json(); //makes the object into JSON object we can use
-    document.getElementById("api-response").textContent = data.description;
-    return data;
+    if (response.status < 200 || response.status > 299) {
+      throw new error(response);
+    } else {
+      const data = await response.json(); //makes the object into JSON object we can use
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `<h2 class="foods"></h2>`
+      );
+      data.creatures.food.forEach((foodName) =>
+        document
+          .querySelector(".foods")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<p>${foodName.name}</p>` <ul>
+              `<p>${foodName.image}</p>``<p>${foodName.description}</p>`</ul>
+          )
+      );
+      data.creatures.non_food.forEach((nonfoodName) =>
+        document
+          .querySelector(".non_food")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<p>${nonfoodName.name}</p>`<ul>`<p>${nonfoodName.image}</p>``<p>${nonfoodName.description}</p>`</ul>
+          )
+      );
+      data.creatures.equipment.forEach((equipmentName) =>
+        document
+          .querySelector(".equipmentNames")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<p>${equipmentName.name}</p>`</ul>`<p>${equipmentName.image}</p>``<p>${equipmentName.description}</p>`</ul>
+          )
+      );
+      data.creatures.materials.forEach((materialsName) =>
+        document
+          .querySelector(".material")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<p>${materialsName.name}</p>`</ul>`<p>${materialsName.image}</p>``<p>${materialsName.description}</p>`</ul>
+          )
+      );
+    }
   } catch (error) {
     console.log(error);
   }
 }
-api = fetch();
-async function fetchData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (err) {
-    getData(URL);
-    const DOMSelectors = {
-      button: document.getElementById("btn"),
-      text: document.getElementById("#text"),
-      input: document.getElementById("input"),
-    };
-    console.log(DOMSelectors.button);
+Data(URL);
+// console.log(await Data(apiURL));
+DOMSelectors.form.addEventListener("submit", function () {
+  addPoster(
+    DOMSelectors.nameInput.value,
+    DOMSelectors.ratingInput.value,
+    DOMSelectors.imageInput.value
+  );
+  DOMSelectors.form.reset();
+  for (let btn of DOMSelectors.remove) {
+    btn.addEventListener("click", function () {
+      this.parentElement.remove();
+    });
   }
-}
-const quote = await fetchData(apiEntry);
-apiResponseDOM.innerHTML = `Quote: ${quote.content}`;
-fetchData();
+});
+
+DOM.form.addEventListener("submit", function () {
+  event.preventDefault();
+  let name = DOM.name.value.toLowerCase();
+  const URL = `https://botw-compendium.herokuapp.com/api/v2/entry/${name}`;
+  Data(URL);
+  clear();
+});
